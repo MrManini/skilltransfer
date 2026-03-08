cc51b55de8c22bb1f606226f2bef71b48f551250
Bryan <bemontoya@uninorte.edu.co>
2026-03-07 19:15:25 -0500

    Add Prisma + Services modules and ESM support

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './app.controller.js';
+import { AppService } from './app.service.js';
+import { PrismaModule } from './prisma/prisma.module.js';
+import { ServicesModule } from './services/services.module.js';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, ServicesModule],
   controllers: [AppController],
   providers: [AppService],
 })
