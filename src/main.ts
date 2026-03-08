a0901c18137b937ca9e7c12cf24033a346791789
kevin <kevintorregrosa2304@gmail.com>
2026-03-07 19:45:56 -0500

    new migration + swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,28 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Skilltransfer API')
+    .setDescription('API for the Skilltransfer backend system')
+    .setVersion('0.1')
+    /*
+    .addBearerAuth(
+      { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
+      'access',
+    )
+    .addBearerAuth(
+      { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
+      'refresh',
+    )
+    */
+    .build();
+    
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
