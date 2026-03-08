d465b46d078ec0181c64068bdd03d915391d91b0
alejarc <alejandrare13@gmail.com>
2026-03-08 12:34:07 -0500

    Cambio de nombre de un archivo

--- src/services/services.module.ts
+++ src/services/services.module.ts
@@ -3,7 +3,7 @@ import { ServicesController } from './controllers/services.controller.js';
 import { BookingController } from './controllers/booking.controller.js';
 import { ServicesService } from './services.service.js';
 import { BookingService } from './booking.service.js';
-import { BookingBridgeController } from './controllers/booking.bridge.controler.js';
+import { BookingBridgeController } from './controllers/booking.bridge.controller.js';
 import { BookingBridgeService } from './booking.bridge.service.js';
 import { BookingBridgeRepository } from './booking.bridge.repository.js';
 
