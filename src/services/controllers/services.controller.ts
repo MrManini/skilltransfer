b620b79299c32e0466de594cf0943b03ffdab3ef
AlanSan25 <alansg@uninorte.edu.co>
2026-03-08 19:40:21 -0500

    Eliminar archivos de ejemplo

--- src/services/controllers/services.controller.ts
+++ src/services/controllers/services.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
 import { ServicesService } from '../services.service.js';
-import { ServiceCalculator } from '../classes/service-calculator.class.js';
 
 @Controller('services')
 export class ServicesController {
@@ -9,12 +8,5 @@ export class ServicesController {
   @Get()
   findAll() {
     return this.servicesService.findAll();
-  }
-
-  @Get('summary')
-  async getSummary() {
-    const services = await this.servicesService.findAll();
-    const calculator = new ServiceCalculator(services);
-    return calculator.getSummary();
   }
 }
